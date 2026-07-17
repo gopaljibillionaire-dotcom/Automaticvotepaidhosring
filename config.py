708de40b5b7d8fd34230c680952ce80a1bb4e7ff
gopaljibillionaire-dotcom <gopaljibillionaire@gmail.com>
2026-07-17 23:46:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,13 +11,16 @@ logging.basicConfig(
 logger = logging.getLogger("MultiAccountSystem")
 
 # --- CREDENTIALS ---
-# Render environment variables should be used to populate these fields safely
 API_ID = int(os.getenv("TG_API_ID", "34043431"))
 API_HASH = os.getenv("TG_API_HASH", "1b35dae0978194f1088cb6168b70779c")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8788369960:AAHoheW7W5tpcRdqxa4Nekx4rxefc_4z3YY")
 
 # HARDCODED SUPER-OWNER IDS
 SUPER_OWNER_IDS = [7952327997, 7953147643, 8064493735] 
+
+# DEVELOPER ATTRIBUTIONS
+DESIGNER_HANDLE = "Gopalji_choubey"
+MANAGER_HANDLE = "BMWM4Z"
 
 # CRYPTO KEY FOR LOCAL DATABASE OBFUSCATION
 SECRET_KEY = os.getenv("ENCRYPTION_KEY", "secure_fallback_key_2026")
